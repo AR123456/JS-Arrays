73f2ea3f722d3c56cd06bc4977e927a81324a5b0
AR123456 <coloradoz99@gmail.com>
2019-02-04 05:25:13 -0700

    helpper functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   render() {
@@ -6,7 +7,14 @@ class StorePicker extends React.Component {
       <form action="" className="store-selector">
         {/* this is how to put in a comment, the comment must be inside this element not before it   */}
         <h2>Please Enter a Store </h2>
-        <input type="text" required placeholder="Store Name" />
+        {/* values of input must be directly attached to state  */}
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          // when loaded defaultValue with run the function getFunName
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
