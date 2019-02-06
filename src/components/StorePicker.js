02ad1cb6d2915da85fb7fa4479727ddf3f974ca4
AR123456 <coloradoz99@gmail.com>
2019-02-06 04:05:44 -0700

    handeling events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,39 +2,31 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  // one way uses a constructor, may be better to use the way below where goToStore was made inot an event
-  // constructor() {
-  //   super();
-  //   this.goToStore = this.goToStore.bind(this);
-  // }
   myInput = React.createRef();
-  // if you need to access "this" inside a custom  method do it this way
+
   goToStore = event => {
-    //1.  stop the form from submitting
+    // 1. Stop the form from submitting
     event.preventDefault();
-    //2.  get text from that input
-    console.log(this);
-    //3. change page to what was entered
+    // 2. get the text from that input
+    const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
   };
   render() {
     return (
-      <form action="" className="store-selector" onSubmit={this.goToStore}>
-        {/* this is how to put in a comment, the comment must be inside this element not before it   */}
-        <h2>Please Enter a Store </h2>
-
-        {/* values of input must be directly attached to state  */}
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
         <input
           type="text"
-          // surface the input on the component
           ref={this.myInput}
           required
           placeholder="Store Name"
-          // when loaded defaultValue with run the function getFunName
           defaultValue={getFunName()}
         />
-        <button type="submit">Visit Store</button>
+        <button type="submit">Visit Store →</button>
       </form>
     );
   }
 }
+
 export default StorePicker;
