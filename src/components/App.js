33f9f5822805debcf7d6fefe95d09bea5aaf8209
AR123456 <coloradoz99@gmail.com>
2019-02-24 05:49:32 -0700

    display order status

--- src/components/App.js
+++ src/components/App.js
@@ -50,7 +50,8 @@ export class App extends Component {
             />
           ))}
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        {/* <Order {...this.state} /> */}
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
