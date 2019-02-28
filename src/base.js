34a95c897b1ff40e211eb48116a40d5aa9979399
AR123456 <coloradoz99@gmail.com>
2019-02-28 05:29:41 -0700

    establishing project

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from "re-base";
-import firebase from "firebase";
-
-const base = Rebase.createClass(firebaseApp.database());
-
-//named export
-export { firebaseApp };
-//this is a default exprot
-export default base;
