991b7d52fc774351d4fec232e4aed5bb617156be
AR123456 <coloradoz99@gmail.com>
2019-03-07 05:02:27 -0700

    component validation

--- src/base.js
+++ src/base.js
@@ -1,11 +1,7 @@
 import Rebase from "re-base";
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAj46jFA0hsLiVe_SuiFNVdUbzi_eZ5324",
-  authDomain: "the-best-ever-cc19c.firebaseapp.com",
-  databaseURL: "https://the-best-ever-cc19c.firebaseio.com"
-});
+const firebaseApp = firebase.initializeApp({});
 const base = Rebase.createClass(firebaseApp.database());
 
 //named export
